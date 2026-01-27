46551efe9f3139435066d090fec16445cc72f5b0
CarlosHa44 <carlosha176@gmail.com>
2026-01-27 12:35:49 -0500

    Feat:Fac test

--- factorial.test.js
+++ factorial.test.js
@@ -4,3 +4,7 @@ const factorial = require('./factorial');
 test('Factorial de 5 debe ser 120', () => {
     expect(factorial(5)).toBe(120);
 });
+
+test('Factorial de 0 debe ser 1', () => {
+    expect(factorial(0)).toBe(1);
+}); 
